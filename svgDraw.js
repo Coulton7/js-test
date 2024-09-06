10ac6535dcbfbe0349debbaa013737094da0dba0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-09-06 11:10:52 +0100

    Resetting the js need to find

--- svgDraw.js
+++ svgDraw.js
@@ -84,44 +84,44 @@ function connectAll() {
     // This script manipulates the SVG lines in product_page.tpl.php to join the stock code <spans> to the drupal field outputs for each build materials bit. 
     // the machine names for the feilds are pretty awful
     
-   if (document.querySelector(".Rotary1")){
+   if (document.querySelector(".Rotary1").length){
     connectElements(document.querySelector("#svg1"), document.querySelector("#rotary1"), document.querySelector(".Rotary1"),   document.querySelector(".field-name-build-materials-primary-rotary"));
     offset +=30;
     };
     
-   if (document.querySelector(".Stationary1")){
+   if (document.querySelector(".Stationary1").length){
     connectElements(document.querySelector("#svg1"), document.querySelector("#stationary1"), document.querySelector(".Stationary1"),   document.querySelector(".field-name-build-materials-primary-stationa"));
     offset +=30;
      };
      
-   if (document.querySelector(".Elastomer1")){
+   if (document.querySelector(".Elastomer1").length){
     connectElements(document.querySelector("#svg1"), document.querySelector("#elastomer1"), document.querySelector(".Elastomer1"),   document.querySelector(".field-name-build-materials-primary-elastome"));
     offset +=50;
           };
    
    
-   if (document.querySelector(".field-name-build-materials-secondary-rotary")){ //Tandem rotary position
+   if (document.querySelector(".field-name-build-materials-secondary-rotary").length){ //Tandem rotary position
     connectElements(document.querySelector("#svg1"), document.querySelector("#rotary2"), document.querySelector(".Rotary2"),   document.querySelector(".field-name-build-materials-secondary-rotary"));
     offset +=40;
      };
      
      
-   if (document.querySelector(".field-name-build--secondary-rotary-standard")){ //Standard rotary position, sorry these machine names are terrible. 
+   if (document.querySelector(".field-name-build--secondary-rotary-standard").length){ //Standard rotary position, sorry these machine names are terrible. 
     connectElements(document.querySelector("#svg1"), document.querySelector("#rotary2"), document.querySelector(".Rotary2"),   document.querySelector(".field-name-build--secondary-rotary-standard"));
     offset -=20;
      };
     
-   if (document.querySelector(".Stationary2")){
+   if (document.querySelector(".Stationary2").length){
     connectElements(document.querySelector("#svg1"), document.querySelector("#stationary2"), document.querySelector(".Stationary2"),   document.querySelector(".field-name-build-materials-secondary-statio"));
     offset -=30;
      };
    
-   if (document.querySelector(".Elastomer2")){
+   if (document.querySelector(".Elastomer2").length){
     connectElements(document.querySelector("#svg1"), document.querySelector("#elastomer2"), document.querySelector(".Elastomer2"),   document.querySelector(".field-name-build-materials-secondary-elasto"));
     offset -=30;
      };
      
-   if (document.querySelector(".build-code")){        
+   if (document.querySelector(".build-code").length){        
     connectElements(document.querySelector("#svg1"), document.querySelector("#build1"), document.querySelector(".build-code"),   document.querySelector(".field-name-build-materials-build-code"));
      };
 }
