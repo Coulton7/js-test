ff9856ca9067072669bc437937650cd91fef51d6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-09-06 10:28:23 +0100

    Adding files for the initial test of comp seals js

---
+++ svgDraw.js
@@ -0,0 +1,142 @@
+//helper functions, it turned out chrome doesn't support Math.sgn() 
+function signum(x) {
+    return (x < 0) ? -1 : 1;
+}
+function absolute(x) {
+    return (x < 0) ? -x : x;
+}
+
+  var offset= 0;
+
+function drawPath(svg, path, startX, startY, endX, endY) {
+    // get the path's stroke width (if one wanted to be  really precize, one could use half the stroke size)
+    var stroke =  parseFloat(path.attr("stroke-width"));
+    // check if the svg is big enough to draw the path, if not, set heigh/width
+    if (svg.attr("height") <  endY)                 svg.attr("height", endY);
+    if (svg.attr("width" ) < (startX + stroke) )    svg.attr("width", (startX + stroke));
+    if (svg.attr("width" ) < (endX   + stroke) )    svg.attr("width", (endX   + stroke));
+    
+    var deltaX = (endX - startX) * 0.15;
+    var deltaY = (endY - startY) * 0.15;
+    // for further calculations which ever is the shortest distance
+    var delta  =  deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+
+    // set sweep-flag (counter/clock-wise)
+    // if start element is closer to the left edge,
+    // draw the first arc counter-clockwise, and the second one clock-wise
+    var arc1 = 0; var arc2 = 1;
+    if (startX > endX) {
+        arc1 = 1;
+        arc2 = 0;
+        
+    }
+    
+  
+    // draw tha pipe-like path
+    // 1. move a bit down, 2. arch,  3. move a bit to the right, 4.arch, 5. move down to the end 
+    path.attr("d",  "M"  + startX + " " + startY +
+                    " V" + (startY + delta + offset) +
+                    " A" + delta + " " +  delta + " 0 0 " + arc1 + " " + (startX + delta*signum(deltaX)) + " " + (startY + 2*delta+ offset) +
+                    " H" + (endX - delta*signum(deltaX)) + 
+                    " A" + delta + " " +  delta + " 0 0 " + arc2 + " " + endX + " " + (startY + 3*delta + offset) +
+                    " V" + endY );
+                    
+                     
+                  
+}
+
+function connectElements(svg, path, startElem, endElem) {
+    var svgContainer= document.querySelector("#svgContainer");
+
+    // if first element is lower than the second, swap!
+    if(startElem.offset().top > endElem.offset().top){
+        var temp = startElem;
+        startElem = endElem;
+        endElem = temp;
+    }
+
+    // get (top, left) corner coordinates of the svg container   
+    var svgTop  = svgContainer.offset().top;
+    var svgLeft = svgContainer.offset().left;
+
+    // get (top, left) coordinates for the two elements
+    var startCoord = startElem.offset();
+    var endCoord   = endElem.offset();
+
+    // calculate path's start (x,y)  coords
+    // we want the x coordinate to visually result in the element's mid point
+    var startX = startCoord.left + 0.5*startElem.outerWidth() - svgLeft;    // x = left offset + 0.5*width - svg's left offset
+    var startY = startCoord.top  + startElem.outerHeight() - svgTop;        // y = top offset + height - svg's top offset
+
+        // calculate path's end (x,y) coords
+    var endX = endCoord.left + 0.5*endElem.outerWidth() - svgLeft;
+    var endY = endCoord.top  - svgTop;
+
+    // call function for drawing the path
+    drawPath(svg, path, startX, startY, endX, endY);
+
+}
+
+
+
+function connectAll() {
+    // connect all the paths you want!
+    // This script manipulates the SVG lines in product_page.tpl.php to join the stock code <spans> to the drupal field outputs for each build materials bit. 
+    // the machine names for the feilds are pretty awful
+    
+   if (document.querySelector(".Rotary1").length){
+    connectElements(document.querySelector("#svg1"), document.querySelector("#rotary1"), document.querySelector(".Rotary1"),   document.querySelector(".field-name-build-materials-primary-rotary"));
+    offset +=30;
+    };
+    
+   if (document.querySelector(".Stationary1").length){
+    connectElements(document.querySelector("#svg1"), document.querySelector("#stationary1"), document.querySelector(".Stationary1"),   document.querySelector(".field-name-build-materials-primary-stationa"));
+    offset +=30;
+     };
+     
+   if (document.querySelector(".Elastomer1").length){
+    connectElements(document.querySelector("#svg1"), document.querySelector("#elastomer1"), document.querySelector(".Elastomer1"),   document.querySelector(".field-name-build-materials-primary-elastome"));
+    offset +=50;
+          };
+   
+   
+   if (document.querySelector(".field-name-build-materials-secondary-rotary").length){ //Tandem rotary position
+    connectElements(document.querySelector("#svg1"), document.querySelector("#rotary2"), document.querySelector(".Rotary2"),   document.querySelector(".field-name-build-materials-secondary-rotary"));
+    offset +=40;
+     };
+     
+     
+   if (document.querySelector(".field-name-build--secondary-rotary-standard").length){ //Standard rotary position, sorry these machine names are terrible. 
+    connectElements(document.querySelector("#svg1"), document.querySelector("#rotary2"), document.querySelector(".Rotary2"),   document.querySelector(".field-name-build--secondary-rotary-standard"));
+    offset -=20;
+     };
+    
+   if (document.querySelector(".Stationary2").length){
+    connectElements(document.querySelector("#svg1"), document.querySelector("#stationary2"), document.querySelector(".Stationary2"),   document.querySelector(".field-name-build-materials-secondary-statio"));
+    offset -=30;
+     };
+   
+   if (document.querySelector(".Elastomer2").length){
+    connectElements(document.querySelector("#svg1"), document.querySelector("#elastomer2"), document.querySelector(".Elastomer2"),   document.querySelector(".field-name-build-materials-secondary-elasto"));
+    offset -=30;
+     };
+     
+   if (document.querySelector(".build-code").length){        
+    connectElements(document.querySelector("#svg1"), document.querySelector("#build1"), document.querySelector(".build-code"),   document.querySelector(".field-name-build-materials-build-code"));
+     };
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // reset svg each time 
+    document.querySelector("#svg1").style.height = "0";
+    document.querySelector("#svg1").style.width = "0";
+    connectAll();
+});
+
+window.addEventListener("resize", function () {
+    // reset svg each time 
+    offset = 0;
+    document.querySelector("#svg1").style.height = "0";
+    document.querySelector("#svg1").style.width = "0";
+    connectAll();
+});
