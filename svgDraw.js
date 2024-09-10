3e459b5856bc0bdf70e3a9d546e660b80c30b251
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-09-10 09:43:20 +0100

    REste got a undertanding now

--- svgDraw.js
+++ svgDraw.js
@@ -8,7 +8,40 @@ function absolute(x) {
 
   var offset= 0;
 
-  function connectElements(svg, path, startElem, endElem) {
+function drawPath(svg, path, startX, startY, endX, endY) {
+    // get the path's stroke width (if one wanted to be  really precize, one could use half the stroke size)
+    var stroke =  parseFloat(path.getAttribute("stroke-width"));
+    // check if the svg is big enough to draw the path, if not, set heigh/width
+    if (svg.getAttribute("height") <  endY)                 svg.setAttribute("height", endY);
+    if (svg.getAttribute("width" ) < (startX + stroke) )    svg.setAttribute("width", (startX + stroke));
+    if (svg.getAttribute("width" ) < (endX   + stroke) )    svg.setAttribute("width", (endX   + stroke));
+    
+    var deltaX = (endX - startX) * 0.15;
+    var deltaY = (endY - startY) * 0.15;
+    // for further calculations which ever is the shortest distance
+    var delta  =  deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
+
+    // set sweep-flag (counter/clock-wise)
+    // if start element is closer to the left edge,
+    // draw the first arc counter-clockwise, and the second one clock-wise
+    var arc1 = 0; var arc2 = 1;
+    if (startX > endX) {
+        arc1 = 1;
+        arc2 = 0;
+        
+    }
+  
+    // draw tha pipe-like path
+    // 1. move a bit down, 2. arch,  3. move a bit to the right, 4.arch, 5. move down to the end 
+    path.setAttribute("d",  "M"  + startX + " " + startY +
+                    " V" + (startY + delta + offset) +
+                    " A" + delta + " " +  delta + " 0 0 " + arc1 + " " + (startX + delta*signum(deltaX)) + " " + (startY + 2*delta+ offset) +
+                    " H" + (endX - delta*signum(deltaX)) + 
+                    " A" + delta + " " +  delta + " 0 0 " + arc2 + " " + endX + " " + (startY + 3*delta + offset) +
+                    " V" + endY );                 
+}
+
+function connectElements(svg, path, startElem, endElem) {
     var svgContainer= document.querySelector("#svgContainer");
 
     // if first element is lower than the second, swap!
@@ -38,40 +71,6 @@ function absolute(x) {
     // call function for drawing the path
     drawPath(svg, path, startX, startY, endX, endY);
 
-}
-
-function drawPath(svg, path, startX, startY, endX, endY) {
-    // get the path's stroke width (if one wanted to be  really precize, one could use half the stroke size)
-    var stroke =  parseFloat(path.getAttribute("stroke-width"));
-    // check if the svg is big enough to draw the path, if not, set heigh/width
-    if (svg.getAttribute("height") <  endY)                 svg.setAttribute("height", endY);
-    if (svg.getAttribute("width" ) < (startX + stroke) )    svg.setAttribute("width", (startX + stroke));
-    if (svg.getAttribute("width" ) < (endX   + stroke) )    svg.setAttribute("width", (endX   + stroke));
-    
-    var deltaX = (endX - startX) * 0.15;
-    var deltaY = (endY - startY) * 0.15;
-    // for further calculations which ever is the shortest distance
-    var delta  =  deltaY < absolute(deltaX) ? deltaY : absolute(deltaX);
-
-    // set sweep-flag (counter/clock-wise)
-    // if start element is closer to the left edge,
-    // draw the first arc counter-clockwise, and the second one clock-wise
-    var arc1 = 0; var arc2 = 1;
-    if (startX > endX) {
-        arc1 = 1;
-        arc2 = 0;
-        
-    }
-    
-  
-    // draw tha pipe-like path
-    // 1. move a bit down, 2. arch,  3. move a bit to the right, 4.arch, 5. move down to the end 
-    path.setAttribute("d",  "M"  + startX + " " + startY +
-                    " V" + (startY + delta + offset) +
-                    " A" + delta + " " +  delta + " 0 0 " + arc1 + " " + (startX + delta*signum(deltaX)) + " " + (startY + 2*delta+ offset) +
-                    " H" + (endX - delta*signum(deltaX)) + 
-                    " A" + delta + " " +  delta + " 0 0 " + arc2 + " " + endX + " " + (startY + 3*delta + offset) +
-                    " V" + endY );                 
 }
 
 function connectAll() {
