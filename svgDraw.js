81b44e2ef7e32aad9bb63f728a1e6f83bf705e6c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2024-10-03 14:28:02 +0100

    Testing new variation of the js to see the result

--- svgDraw.js
+++ svgDraw.js
@@ -1,8 +1,8 @@
 function getOffset(el) {
     var rect = el.getBoundingClientRect();
     return {
-      left: rect.left + window.pageXOffset,
-      top: rect.top + window.pageYOffset,
+      left: rect.left + window.scrollX,
+      top: rect.top + window.scrollY,
       width: rect.width || el.offsetWidth,
       height: rect.height || el.offsetHeight
     };
